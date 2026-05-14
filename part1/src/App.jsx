91fa0b12a6cecd02c98f20d5c18012f49fd6b710
Federico Rosciano <federosciano@gmail.com>
2026-05-14 10:25:13 -0300

    ejercicios p1 1.1 y 1.2

---
+++ part1/src/App.jsx
@@ -0,0 +1,59 @@
+
+const App = () => {
+
+
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const Header = () => {
+    return (
+      <div>
+        <p>Curso {course}</p>
+      </div>
+    )
+  }
+
+  const Content = () => {
+    return (
+      <div>
+        <Part name={part1} exercises={exercises1} />
+        <Part name={part2} exercises={exercises2} />
+        <Part name={part3} exercises={exercises3} />
+      </div>
+    )
+  }
+
+  const Part = ({ name, exercises }) => {
+    return (
+      <div>
+        <p>
+          Nombre: {name} Ejercicios: {exercises}
+        </p>
+      </div>
+    )
+  }
+
+
+  const Total = () => {
+    return (
+      <div>
+        <p>Numeros de Ejercicios: {exercises1 + exercises2 + exercises3}</p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total />
+    </div>
+  )
+}
+
+export default App
