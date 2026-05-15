ecba3c56273e5fa51926569bf1313390564b63e8
Federico Rosciano <federosciano@gmail.com>
2026-05-15 13:52:28 -0300

    envio de ejercicio 1.6, 1.7, 1.8, 1.9, 1.10, 1.11, 1.12, 1.13 y 1.14

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,3 +1,46 @@
+import { useState } from "react"
+
+const Button = (props) => {
+  console.log(props)
+  return (
+    <button onClick={props.onClick}>
+      {props.children}
+    </button>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+    <div>
+      {props.text}: {props.value}
+    </div>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+
+  const average =
+    total === 0
+      ? 0
+      : (good - bad) / total
+
+  const percentage =
+    total === 0
+      ? 0
+      : ((good / total) * 100).toFixed(1)
+
+  return (
+    <div>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="total" value={total} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={`${percentage}%`} />
+    </div>
+  )
+}
 
 const App = () => {
 
@@ -57,12 +100,85 @@ const App = () => {
     )
   }
 
+
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+
+  const anecdotas = [
+    "Un buen programador es alguien que siempre está aprendiendo y mejorando.",
+    "La mejor manera de aprender a programar es programando.",
+    "La programación es el arte de hacer que las computadoras hagan lo que tú quieres que hagan.",
+    "No te preocupes si no entiendes algo de inmediato, la programación es un proceso de aprendizaje continuo."
+  ]
+
+  const [selected, setSelected] = useState(Math.floor(Math.random() * anecdotas.length))
+  const [votes, setVotes] = useState(
+    Array(anecdotas.length).fill(0)
+  )
+
+
+  const nextAnecdota = () => {
+    setSelected((selected + 1) % anecdotas.length)
+  }
+
+  const vote = (selected) => {
+    const copy = [...votes]
+
+    copy[selected] += 1
+
+    setVotes(copy)
+  }
+
   return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
+    <>
+      <div>
+        <Header course={course} />
+        <Content course={course} />
+        <Total course={course} />
+      </div>
+
+      <div>
+        <Button onClick={() => setGood(good + 1)}>Good {good}</Button>
+        <Button onClick={() => setNeutral(neutral + 1)}>Neutral {neutral}</Button>
+        <Button onClick={() => setBad(bad + 1)}>Bad {bad}</Button>
+      </div>
+
+      <div>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+        />
+      </div>
+
+      <div>
+        <p>{anecdotas[selected]}</p>
+
+        <p>Votos: {votes[selected]}</p>
+
+        <Button onClick={nextAnecdota}>Siguiente Anecdota</Button>
+        <Button onClick={() => vote(selected)}>Votar</Button>
+      </div>
+
+
+      <div>
+        <h2>Anecdota con más votos</h2>
+        {Math.max(...votes) === 0 ? (
+          <p>No hay votos aún</p>
+        ) : (
+          <div>
+            <p>{anecdotas[votes.indexOf(Math.max(...votes))]}</p>
+            <p>Votos: {Math.max(...votes)}</p>
+          </div>
+        )}
+      </div>
+
+
+    </>
   )
 }
 
